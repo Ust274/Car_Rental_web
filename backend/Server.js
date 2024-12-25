1e9de7f8cdf24553cd02d674ef32972977ddb9e2
your.user.name <Utv4484@gmail.com>
2024-12-25 17:47:22 +0530

    new logo added

--- backend/Server.js
+++ backend/Server.js
@@ -21,7 +21,7 @@ app.use(cors({
 
 //MySQL connection
 const db = mysql.createConnection({
-    host: process.env.DB_HOST || 'localhost',
+    host: process.env.DB_HOST || '127.0.0.1',
     port: process.env.DB_PORT || 3306,
     user: process.env.DB_USER || 'root',
     password: process.env.DB_PASSWORD || '',
